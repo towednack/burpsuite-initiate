88cbf467c101b28e6ff21539bca0711ce95eb304
Towednack <53052236+towednack@users.noreply.github.com>
2025-06-17 14:22:04 -0300

    Update initiate.sh

--- initiate.sh
+++ initiate.sh
@@ -1,7 +1,19 @@
-# Execute Burpsuite Professional
+#!/bin/bash
+
+# Cloning
+git clone https://github.com/xiv3r/Burpsuite-Professional.git 
 cd Burpsuite-Professional
+
+version=2025
+url="https://portswigger-cdn.net/burp/releases/download?product=pro&type=Jar"
+
+# Execute Key Generator
+echo "Starting Key loader.jar..."
+(java -jar loader.jar) &
+
+# Execute Burpsuite Professional
 echo "Executing Burpsuite Professional..."
-echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_v2025.jar &" > burpsuitepro
+echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_v$version.jar &" > burpsuitepro
 chmod +x burpsuitepro
 cp burpsuitepro /bin/burpsuitepro
 (./burpsuitepro)
